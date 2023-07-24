04fb91b4b9168cf8d7e182756da0b2bdb8a9d187
vitcool@ukr.net <vitcool@ukr.net>
2023-07-24 23:24:44 +0300

    GS-0: test apollo client is added + request to viewer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// apollo
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+// end apollo
+
+import config from './config/index.ts';
+
+import App from './App.tsx';
+
+import './index.css';
+
+const httpLink = createHttpLink({
+  uri: config.apiUrl,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const { token } = config;
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query Viewer {
+        viewer {
+          id
+          name
+        }
+      }
+    `,
+  })
+  .then((result) => console.log('result', result));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
